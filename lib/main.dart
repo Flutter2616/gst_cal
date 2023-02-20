902a63aff0199db40ff78698ffb6ca6b0d9bd1cd
prince <princerawal225@gmail.com>
2023-02-21 00:07:37 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:gst_cal/screen/gst_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Gst(),
+    ),
+  );
+}
